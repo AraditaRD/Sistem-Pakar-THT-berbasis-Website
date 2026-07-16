cfeda9b2d58caa9c6094213eadcfc29644d20dae
AraditaRD <araditaratudisanda@gmail.com>
2026-07-16 09:59:38 +0700

    add diagnosa colums to konsultasi

---
+++ database/migrations/2026_07_16_025834_add_diagnosa_columns_to_konsultasi_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('konsultasi', function (Blueprint $table) {
+
+            $table->foreignId('penyakit_id')
+                ->nullable()
+                ->after('status')
+                ->constrained('penyakit')
+                ->cascadeOnDelete();
+
+            $table->decimal('persentase', 5, 2)
+                ->default(0)
+                ->after('penyakit_id');
+
+            $table->json('kemungkinan_lain')
+                ->nullable()
+                ->after('persentase');
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('konsultasi', function (Blueprint $table) {
+
+            $table->dropForeign(['penyakit_id']);
+
+            $table->dropColumn([
+                'penyakit_id',
+                'persentase',
+                'kemungkinan_lain'
+            ]);
+
+        });
+    }
+};
